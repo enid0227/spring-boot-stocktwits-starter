8b692da5976769d7ab82d23be250d506077453e8
Ying Shih <enid0227@gmail.com>
2021-07-07 19:29:59 -0700

    init commit from spring-boot initializer

---
+++ src/main/java/com/example/stocktwitsdemo/StocktwitsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.stocktwitsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocktwitsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocktwitsDemoApplication.class, args);
+	}
+
+}
